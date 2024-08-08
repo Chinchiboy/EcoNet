548082a2fabdea68e826acca6c022bbd52f1fbe3
Chinchiboy <ivan.espinosa.joliver@gmail.com>
2024-08-08 12:35:28 +0200

    Agregar archivos de proyecto.

---
+++ EcoNet/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EcoNet.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
