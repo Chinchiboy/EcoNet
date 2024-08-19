e72b00f1af9cfc5f25fe8f55feeeca846b9e9488
SergioGutierrezSantome <gutierrez.sergio.360@gmail.com>
2024-08-19 13:19:18 +0200

    Controlador de CrearAnuncio

---
+++ EcoNet/Controllers/CrearAnuncioController.cs
@@ -0,0 +1,35 @@
+﻿using EcoNet.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EcoNet.Controllers
+{
+    public class CrearAnuncioController : Controller
+    {
+        private readonly DalAnuncio Dalanuncio;
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult CrearAnuncio()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CrearAnuncio(int idAnuncio,string titulo,byte[]? imagen,string descripcion,decimal precio,int? fkborradoPor, int fkusuario,bool estaVendido)
+        {
+            Anuncio nuevoAnuncio = new Anuncio
+            {
+                IdAnuncio = idAnuncio,
+                Titulo = titulo,
+                Imagen = imagen, 
+                Descripcion = descripcion,
+                Precio = precio,
+                FkborradoPor = fkborradoPor, 
+                Fkusuario = fkusuario,
+                EstaVendido = estaVendido
+            };
+            Dalanuncio.Add(nuevoAnuncio);
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
