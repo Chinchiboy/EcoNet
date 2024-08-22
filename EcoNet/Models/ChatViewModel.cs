e8420b7c8382bbf942b1a38257b0c50fa1f958d9
cram245 <marcalbaf3@gmail.com>
2024-08-22 13:30:13 +0200

    mejoras chat

---
+++ EcoNet/Models/ChatViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace EcoNet.Models
+{
+    public class ChatViewModel
+    {
+        public Chat Chat { get; set; }
+        public string NombreDestinatario { get; set; }
+        public string TituloAnuncio { get; set; }
+        public ChatViewModel() { }
+    }
+}
