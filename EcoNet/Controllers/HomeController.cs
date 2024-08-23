dede4552091a93f0318581e907de5ef788b6a221
gorkaesparrech <esparrechgorka@gmail.com>
2024-08-23 09:58:52 +0200

    Sobre Nosotros Funcional

--- EcoNet/Controllers/HomeController.cs
+++ EcoNet/Controllers/HomeController.cs
@@ -38,6 +38,11 @@ namespace EcoNet.Controllers
             return RedirectToAction("Index");
         }
 
+        public IActionResult Nosotros()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
